5f3729f664e017a7bcaf2d267a33a672ab4be464
David <davo.melkumyan40@gmail.com>
2019-10-18 16:10:00 +0200

    Add project files.

---
+++ NaturallySpawner/ModelContainer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+using System.Windows.Forms;
+
+namespace NaturallySpawner
+{
+    public class ModelContainer
+    {
+        public bool IsFirstTime { get; private set; }
+        public bool IsCreated { get; private set; }
+        public VehicleHash Hash { get; private set; }
+        public Vehicle Vehicle { get; private set; }
+        public Vector3 Position { get; private set; }
+        public Vector3 Rotation { get; private set; }
+        public VehicleColor Primarry { get; private set; }
+        public VehicleColor Secondarry { get; private set; }
+
+        public ModelContainer(VehicleHash hash, Vector3 position, Vector3 rotation, VehicleColor primarry, VehicleColor secondarry)
+        {
+            this.Hash = hash;
+            this.Position = position;
+            this.Rotation = rotation;
+            this.Primarry = primarry;
+            this.Secondarry = secondarry;
+            this.IsCreated = false;
+            this.IsFirstTime = true;
+        }
+
+        public ModelContainer(VehicleHash hash, Vector3 position, Vector3 rotation)
+        {
+            this.Hash = hash;
+            this.Position = position;
+            this.Rotation = rotation;
+            this.Primarry = this.Secondarry = VehicleColor.DefaultAlloyColor;
+            this.IsCreated = false;
+            this.IsFirstTime = true;
+        }
+
+        public void Create()
+        {
+            Vector3 player = Game.Player.Character.GetOffsetInWorldCoords(new Vector3(0, 0, 0));
+            if (!IsCreated)
+            {
+                if (IsFirstTime)
+                {
+                    //let player = {X:140, Y:-735, Z: 33}
+                    //VP = SQRT( (player.X^2 - ( 2 * player.X * vehicle.X ) + vehicle.X^2 )^2 + (player.X^2 - ( 2 * player.X * vehicle.X ) + vehicle.X^2 )^2 + (player.X^2 - ( 2 * player.X * vehicle.X ) + vehicle.X^2 )^2
+                    if (Math.Sqrt(
+                        (Math.Pow(Math.Pow(player.X, 2) - (2 * player.X * this.Position.X) + Math.Pow(this.Position.X, 2), 2)) +
+                        (Math.Pow(Math.Pow(player.Y, 2) - (2 * player.Y * this.Position.Y) + Math.Pow(this.Position.Y, 2), 2)) +
+                        (Math.Pow(Math.Pow(player.Z, 2) - (2 * player.Z * this.Position.Z) + Math.Pow(this.Position.Z, 2), 2))) < 100000) // 100000cm = 1000m = 1km
+                    {
+                        IsFirstTime = false;
+                        OnCreate();
+                        UI.Notify("Debug(Created)");
+                    }
+                }
+                else if (Math.Sqrt(
+                        (Math.Pow(Math.Pow(player.X, 2) - (2 * player.X * this.Vehicle.Position.X) + Math.Pow(this.Vehicle.Position.X, 2), 2)) +
+                        (Math.Pow(Math.Pow(player.Y, 2) - (2 * player.Y * this.Vehicle.Position.Y) + Math.Pow(this.Vehicle.Position.Y, 2), 2)) +
+                        (Math.Pow(Math.Pow(player.Z, 2) - (2 * player.Z * this.Vehicle.Position.Z) + Math.Pow(this.Vehicle.Position.Z, 2), 2))) < 100000)
+                {
+                    OnCreate();
+                    UI.Notify("Debug(Created)");
+                }
+            }
+            else if (Math.Sqrt(
+                        (Math.Pow(Math.Pow(player.X, 2) - (2 * player.X * this.Vehicle.Position.X) + Math.Pow(this.Vehicle.Position.X, 2), 2)) +
+                        (Math.Pow(Math.Pow(player.Y, 2) - (2 * player.Y * this.Vehicle.Position.Y) + Math.Pow(this.Vehicle.Position.Y, 2), 2)) +
+                        (Math.Pow(Math.Pow(player.Z, 2) - (2 * player.Z * this.Vehicle.Position.Z) + Math.Pow(this.Vehicle.Position.Z, 2), 2))) > 100000)
+            {
+                Delete();
+            }
+        }
+
+        private void Delete()
+        {
+            this.Vehicle.Delete();
+            this.IsCreated = false;
+            this.IsFirstTime = true;
+            UI.Notify("Debug(Deleted)");
+
+        }
+
+        private void OnCreate()
+        {
+            this.Vehicle = World.CreateVehicle(new Model(this.Hash), this.Position);
+            this.Vehicle.Rotation = this.Rotation;
+            if (this.Primarry != VehicleColor.DefaultAlloyColor)
+                this.Vehicle.PrimaryColor = this.Primarry;
+            if (this.Secondarry != VehicleColor.DefaultAlloyColor)
+                this.Vehicle.SecondaryColor = this.Secondarry;
+            this.IsCreated = true;
+        }
+    }
+}
---
+++ NaturallySpawner/NaturSpawner.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+using System.Windows.Forms;
+
+namespace NaturallySpawner
+{
+    public class NaturSpawner : Script
+    {
+        private ModelContainer[] modelList;
+
+        public NaturSpawner()
+        {
+            modelList = new ModelContainer[]
+            {
+            //Millitary
+            new ModelContainer(VehicleHash.APC, new Vector3(-2454, 2979, 32), new Vector3(0, 0, -79), VehicleColor.DefaultAlloyColor, VehicleColor.DefaultAlloyColor),
+            new ModelContainer(VehicleHash.HalfTrack, new Vector3(-2455, 2985, 32), new Vector3(0, 0, -79)),
+            new ModelContainer(VehicleHash.Brickade, new Vector3(-2452, 2954, 33), new Vector3(0, 0, -35)),
+            new ModelContainer(VehicleHash.Frogger, new Vector3(300, -1452, 46), new Vector3(0, 0, 143), VehicleColor.MetallicWhite, VehicleColor.MetallicWhite),
+            new ModelContainer(VehicleHash.Frogger, new Vector3(300, -1452, 46), new Vector3(0, 0, 143), VehicleColor.MetallicWhite, VehicleColor.MetallicWhite),
+            new ModelContainer(VehicleHash.Cargobob, new Vector3(-1859, 2795, 33), new Vector3(0, 0, 153)),
+            new ModelContainer(VehicleHash.Lazer, new Vector3(-1882, 3100, 33), new Vector3(0, 0, 151)),
+            new ModelContainer(VehicleHash.Molotok, new Vector3(-2607, 3307, 33), new Vector3(0, 0, -124)),
+            new ModelContainer(VehicleHash.Valkyrie, new Vector3(-1973, 2814, 33), new Vector3(0, 0, -125)),
+            new ModelContainer(VehicleHash.Havok, new Vector3(-2097, 2822, 39), new Vector3(0, 0, -9), VehicleColor.MetallicRed, VehicleColor.MetallicRed),
+            new ModelContainer(VehicleHash.Bombushka, new Vector3(-2004, 2856, 33), new Vector3(0, 0, 62)),
+            //city
+            new ModelContainer(VehicleHash.Nero, new Vector3(-1033, -490, 36), new Vector3(0, 0, -154)),
+            new ModelContainer(VehicleHash.GP1, new Vector3(-448, -458, 32), new Vector3(0, 0, -13)),
+            new ModelContainer(VehicleHash.Reaper, new Vector3(-141, -594, 32), new Vector3(0, 0, -115)),
+            new ModelContainer(VehicleHash.SultanRS, new Vector3(478, -1893, 25), new Vector3(0, 0, -159)),
+            new ModelContainer(VehicleHash.Tempesta, new Vector3(1027, -121, 73), new Vector3(0, 0, -51)),
+            new ModelContainer(VehicleHash.XA21, new Vector3(824, 1175, 323), new Vector3(0, 0, 167)),
+            new ModelContainer(VehicleHash.Visione, new Vector3(689, 72, 83), new Vector3(0, 0, 61)),
+            new ModelContainer(VehicleHash.Elegy, new Vector3(529, -28, 69), new Vector3(0, 0, 35)),
+            new ModelContainer(VehicleHash.Seven70, new Vector3(253, 75, 99), new Vector3(0, 0, -25)),
+            new ModelContainer(VehicleHash.Nightshade, new Vector3(174, 471, 141), new Vector3(0, 0, -18)),
+            new ModelContainer(VehicleHash.Cognoscenti2, new Vector3(-302, -742, 38), new Vector3(0, 0, 163)),
+            new ModelContainer(VehicleHash.Police4, new Vector3(146, -695.5f, 32.7f), new Vector3(0, 0, -113.7f)),
+            new ModelContainer(VehicleHash.FBI2, new Vector3(141, -744, 32.7f), new Vector3(0, 0, -24)),
+            new ModelContainer(VehicleHash.Submersible2, new Vector3(506, -3387, 7), new Vector3(0, 0, 183)),
+            new ModelContainer(VehicleHash.Cargobob2, new Vector3(-1145, -2864, 14), new Vector3(0, 0, 155)),
+            new ModelContainer(VehicleHash.Supervolito, new Vector3(-1178, -2846, 14), new Vector3(0, 0, 155)),
+            new ModelContainer(VehicleHash.XLS2, new Vector3(-978, -2938, 14), new Vector3(0, 0, 124)),
+            new ModelContainer(VehicleHash.Khamelion, new Vector3(485, -1082, 28), new Vector3(0, 0, 92)),
+            new ModelContainer(VehicleHash.CargoPlane, new Vector3(-1229.4f, -2266, 19.3f), new Vector3(0, 0, 64.7f)),
+            new ModelContainer(VehicleHash.AlphaZ1, new Vector3(-1672, -3114, 13.7f), new Vector3(0, 0, 129.4f)),
+            new ModelContainer(VehicleHash.Pfister811, new Vector3(-181.3f, 171, 69.6f), new Vector3(0, 0, 179.7f)),
+        };
+            this.Interval = 1000;
+            Tick += NaturallySpawner_Tick;
+        }
+
+
+        private void NaturallySpawner_Tick(object sender, EventArgs e)
+        {
+            if (!Game.IsPaused)
+            {
+                try
+                {
+                    foreach (var model in modelList)
+                    {
+                        model.Create();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    UI.Notify("Something's goin wrong: " + ex.Message);
+                }
+            }
+        }
+    }
+}
